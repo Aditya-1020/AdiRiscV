1863da635815f9347b67b75b3934aa492ed847a2
Aditya-1020 <adityareddy400@gmail.com>
2026-01-17 21:04:40 +0530

    RV32IM

--- sim_build/cmds.f
+++ sim_build/cmds.f
@@ -1,58 +1,66 @@
+
 # Package
 rtl/core/riscv_pkg.sv
 
-# Core Top Level
-# rtl/core/riscv_single_cycle_core.sv
-# tb/tb_single_cycle.sv
-rtl/core/riscv_pipelined_core.sv
-# tb/tb_pipelined.sv
 
-# ALU + M Extension
-rtl/backend/execute/alu.sv
-rtl/backend/execute/divider.sv
-# tb/tb_alu.sv
-tb/tb_mtype.sv
+# Frontend — Fetch
+rtl/frontend/fetch/pc.sv
+rtl/frontend/fetch/imem.sv
+rtl/frontend/fetch/if_stage.sv
 
-# Branch Unit
-rtl/backend/execute/branch_unit.sv  
-# tb/tb_branch_unit.sv
-
-# Data Memory
-rtl/backend/memory/dmem.sv  
-# tb/tb_dmem.sv
-
-# Decoder
-rtl/frontend/decode/decoder.sv  
-# tb/tb_decoder.sv
-
-# Immediate Generator
-rtl/frontend/decode/imm_gen.sv  
-# tb/tb_imm_gen.sv
-
-# Register File
-rtl/frontend/decode/regfile.sv  
-# tb/tb_regfile.sv
-
-# Instruction Memory
-rtl/frontend/fetch/imem.sv  
+# tb/tb_pc.sv
+# tb/tb_pc_plus4.sv
 # tb/tb_imem.sv
 
-# Program Counter
-rtl/frontend/fetch/pc.sv  
-# tb/tb_pc.sv  
-# tb/tb_pc_plus4.sv
+
+# Frontend — Decode
+rtl/frontend/decode/imm_gen.sv
+rtl/frontend/decode/regfile.sv
+rtl/frontend/decode/decoder.sv
+rtl/frontend/decode/id_stage.sv
+
+# tb/tb_imm_gen.sv
+# tb/tb_regfile.sv
+# tb/tb_decoder.sv
+
+
+# Backend — Execute
+rtl/backend/execute/alu.sv
+rtl/backend/execute/divider.sv
+rtl/backend/execute/branch_unit.sv
+rtl/backend/execute/ex_stage.sv
+
+# tb/tb_alu.sv
+# tb/tb_mtype.sv
+# tb/tb_branch_unit.sv
+
+
+# Backend — Memory
+rtl/backend/memory/dmem.sv
+rtl/backend/memory/mem_stage.sv
+
+# tb/tb_dmem.sv
+
+
+# Backend — Writeback
+rtl/backend/writeback/wb_stage.sv
+
 
 # Pipeline Registers
-rtl/pipeline/ex_mem_reg.sv
-rtl/pipeline/mem_wb_reg.sv
 rtl/pipeline/if_id_reg.sv
 rtl/pipeline/id_ex_reg.sv
+rtl/pipeline/ex_mem_reg.sv
+rtl/pipeline/mem_wb_reg.sv
+
+
+# Pipeline Control Units
 rtl/pipeline/forwarding_unit.sv
 rtl/pipeline/hazard_unit.sv
 
-# Pipeline Stages
-rtl/frontend/fetch/if_stage.sv
-rtl/frontend/decode/id_stage.sv
-rtl/backend/memory/mem_stage.sv
-rtl/backend/execute/ex_stage.sv
-rtl/backend/writeback/wb_stage.sv
+
+# Core Top Level
+rtl/core/riscv_single_cycle_core.sv
+rtl/core/riscv_pipelined_core.sv
+
+# tb/tb_single_cycle.sv
+# tb/tb_pipelined.sv
